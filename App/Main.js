99f5ad131572d41dcbc6331212f671e86ac9aec2
Nofrets Poai <opetstudio@gmail.com>
2018-05-31 16:58:59 +0700

    add App

---
+++ App/Main.js
@@ -0,0 +1,8 @@
+import '../App-JemaatApp/App/Config/ReactotronConfig'
+import App from '../App-JemaatApp/App/Containers/App'
+import { registerHeadlessListener } from '../App-JemaatApp/App/Listeners'
+
+export default {
+    App,
+    registerHeadlessListener
+}
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// import './App/Config/ReactotronConfig'
+import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import Main from './App/Main'
 
-// import {registerHeadlessListener} from './App/Listeners';
-// import App from './App/Main'
-const App = Main.App
-const registerHeadlessListener = Main.registerHeadlessListener
+import {registerHeadlessListener} from './App/Listeners';
+import App from './App/Main'
+// const App = Main.App
+// const registerHeadlessListener = Main.registerHeadlessListener
 
 AppRegistry.registerComponent('PizzaApp', () => App)
 AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => registerHeadlessListener);
