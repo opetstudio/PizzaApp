c34dcc5fcc1d8057561f6e0a23fe8e5142c0b45d
Nofrets Poai <opetstudio@gmail.com>
2018-05-31 14:34:02 +0700

    add file java

---
+++ android/app/src/main/java/com/opetstudio/jemaatapp/MainApplication.java
@@ -0,0 +1,84 @@
+package com.opetstudio.jemaatapp;
+
+import android.app.Application;
+import android.support.multidex.MultiDexApplication;
+
+import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+//import com.evollu.react.fcm.FIRMessagingPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.microsoft.codepush.react.CodePush;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.opetstudio.jemaatapp.BuildConfig;
+import com.opetstudio.jemaatapp.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends MultiDexApplication implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNFirebasePackage(),
+              new RNFirebaseNotificationsPackage(),
+            new RNFirebaseMessagingPackage(),
+              new RNFirebaseDatabasePackage(),
+              new RNFirebaseAuthPackage(),
+//            new FIRMessagingPackage(),
+            new SplashScreenReactPackage(),
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),
+            new RNI18nPackage(),
+            new ReactNativeConfigPackage(),
+            new VectorIconsPackage(),
+            new RNDeviceInfo()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
