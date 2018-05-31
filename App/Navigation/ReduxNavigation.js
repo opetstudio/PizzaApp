24d53d9ef34cf34cc9049814a9cd4549fea67871
Nofrets Poai <opetstudio@gmail.com>
2018-05-31 17:40:34 +0700

    back press

--- App/Navigation/ReduxNavigation.js
+++ App/Navigation/ReduxNavigation.js
@@ -8,10 +8,24 @@ import AppNavigation from './AppNavigation'
 class ReduxNavigation extends React.Component {
   componentWillMount () {
     if (Platform.OS === 'ios') return
+    // BackHandler.addEventListener('hardwareBackPress', () => {
+    //   const { dispatch, nav } = this.props
+    //   // change to whatever is your first screen, otherwise unpredictable results may occur
+    //   if (nav.routes.length === 1 && (nav.routes[0].routeName === 'LaunchScreen')) {
+    //     return false
+    //   }
+    //   // if (shouldCloseApp(nav)) return false
+    //   dispatch({ type: 'Navigation/BACK' })
+    //   return true
+    // })
     BackHandler.addEventListener('hardwareBackPress', () => {
       const { dispatch, nav } = this.props
+      console.log('this.props', this.props)
+      console.log('nav.routes[0].routeName', nav.routes[0].routeName)
+      console.log('nav.routes.length', nav.routes.length)
       // change to whatever is your first screen, otherwise unpredictable results may occur
-      if (nav.routes.length === 1 && (nav.routes[0].routeName === 'LaunchScreen')) {
+      // if (nav.routes.length === 1 && (nav.routes[0].routeName === 'LaunchScreen')) {
+      if (nav.routes.length > 1 && (nav.routes[0].index === 0 && nav.index === 0)) {
         return false
       }
       // if (shouldCloseApp(nav)) return false
