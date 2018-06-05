af3e5d99303516ba19384dd0a2bd3e5f9f5053d1
Nofrets Poai <opetstudio@gmail.com>
2018-06-05 16:31:00 +0700

    edit theme

--- App/Config/AppConfig.js
+++ App/Config/AppConfig.js
@@ -6,7 +6,7 @@ export default {
   // font scaling override - RN default is on
   allowTextFontScaling: true,
   bannerAdUnitID: __DEV__ ? 'ca-app-pub-3940256099942544/6300978111' : 'ca-app-pub-3940256099942544/6300978111',
-  contributorSpace: 'Oleh Nofrets Poai. IG:@opetstudio, FB:nofrets poai, Tw:@opetstudio. Salam untuk jemaat Prisma, Tuhan memberkati.',
+  contributorSpace: 'Data Entry By Nofrets Poai. IG:@opetstudio, FB:nofrets poai, Tw:@opetstudio. Salam untuk jemaat Prisma, Tuhan memberkati.',
   getContributorSpace: (contributorSpace) => {
     return `<div style="border: solid blue 2px; padding: 5px;">${contributorSpace}</div>`
   }
